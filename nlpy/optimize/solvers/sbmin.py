93b1bb547be4c5ceba7116d8088244bea6d1d74b
Andrew Lambe <lambe@utias.utoronto.ca>
2012-09-12 11:24:36 -0400

    Found a bug in the definition of convergence: omega is an absolute tolerance.

--- nlpy/optimize/solvers/sbmin.py
+++ nlpy/optimize/solvers/sbmin.py
@@ -97,7 +97,7 @@ class SBMINFramework:
 
         self.reltol  = kwargs.get('reltol', 1.0e-5)
         self.abstol  = kwargs.get('abstol', 1.0e-7)
-        self.maxiter = kwargs.get('maxiter', 2*self.nlp.n)
+        self.maxiter = kwargs.get('maxiter', max(100,2*self.nlp.n))
         self.verbose = kwargs.get('verbose', True)
         self.total_bqpiter = 0
 
