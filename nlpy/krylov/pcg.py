Reverted to correct file versions before bad merge + fixed bug in AugLag driver to use the correct size for approximate Hessians in partial and total quasi-Newton approaches.

nlpy/krylov/pcg.py
@@ -12,7 +12,7 @@ truncated preconditioned conjugate gradient algorithm as described in
 from nlpy.tools.exceptions import UserExitRequest
 from nlpy.tools.utils import NullHandler
 import numpy as np
-import logging, ipdb
+import logging
 from math import sqrt
 import sys
 
@@ -68,8 +68,8 @@ class TruncatedCG:
 
         # Formats for display
         self.hd_fmt = ' %-5s  %9s  %8s'
-        self.header = self.hd_fmt % ('Iter', '<r,g>', 'curv')
-        self.fmt = ' %-5d  %9.2e  %8.2e'
+        self.header = self.hd_fmt % ('Iter', 'Residual', 'Curvature')
+        self.fmt = ' %-5d  %9.2e  %9.2e'
 
         # Create a logger for solver.
         self.log = logging.getLogger('nlpy.pcg')
@@ -137,14 +137,17 @@ class TruncatedCG:
         if 's0' in kwargs:
             s = kwargs['s0']
             snorm2 = np.linalg.norm(s)
-            r += H*s                 # r = g + H s0
+            Hs = H * s
+            r += Hs                 # r = g + H s0
+            Hs *= 0.5
+            Hs += g
+            self.qval = np.dot(s, Hs)
         else:
             s = np.zeros(n)
             snorm2 = 0.0
+            self.qval = 0.0
         y = prec(r)
         ry = np.dot(r, y)
-
-        exitOptimal = exitIter = exitUser = False
 
         try:
             sqrtry = sqrt(ry)
@@ -154,6 +157,9 @@ class TruncatedCG:
             raise ValueError, msg
 
         stopTol = max(abstol, reltol * sqrtry)
+
+        exitOptimal = sqrtry <= stopTol
+        exitIter = exitUser = False
 
         # Initialize r as a copy of g not to alter the original g
         if 'p0' in kwargs:
@@ -178,7 +184,7 @@ class TruncatedCG:
             pHp = np.dot(p, Hp)
 
             # if debug:
-            self.log.info(self.fmt % (k, ry, pHp))
+            self.log.info(self.fmt % (k, sqrtry, pHp))
 
             # Compute steplength to the boundary.
             if radius is not None:
@@ -195,6 +201,7 @@ class TruncatedCG:
                 self.status = 'infinite descent'
                 snorm2 = 0
                 self.dir = p
+                self.pHp = pHp
                 infDescent = True
                 continue
 
@@ -206,6 +213,9 @@ class TruncatedCG:
                 self.status = 'trust-region boundary active'
                 onBoundary = True
                 continue
+
+            # Update objective function value.
+            self.qval += alpha * np.dot(r, p) + 0.5 * alpha * alpha * pHp
 
             # Move to next iterate.
             s += alpha * p
@@ -220,10 +230,9 @@ class TruncatedCG:
             else:
                 beta = ry_next/ry
 
-            try:
-                p = -y + beta * p
-            except:
-                pdb.set_trace()
+            #p = -y + beta * p
+            p *= beta
+            p -= y
 
             ry = ry_next
 
@@ -246,7 +255,7 @@ class TruncatedCG:
             self.beta = beta
             self.ry = ry
             self.alpha = alpha
-            self.qval = model_value(H,g,s)
+            #self.qval = model_value(H,g,s)
             self.iter = k
 
             try:
@@ -260,8 +269,8 @@ class TruncatedCG:
 
         # Output info about the last iteration.
         # if debug:
-        self.log.info(self.fmt % (k, ry, pHp))
-        self.log.debug('qval: %6.2e' % model_value(H,g,s))
+        #self.log.info(self.fmt % (k, ry, pHp))
+        #self.log.debug('qval: %6.2e' % self.qval)
         if k < maxiter and not onBoundary and not infDescent and not exitUser:
             self.status = 'residual small'
         elif k >= maxiter:

nlpy/optimize/solvers/auglag2.py
@@ -123,6 +123,7 @@ class AugmentedLagrangian(NLPModel):
                                     matvec= lambda u: self.hprod(x,z,u))
 
 
+
 class AugmentedLagrangianQuasiNewton(AugmentedLagrangian):
     """
     Use Quasi Newton approximate Hessian instead of true Hessian.
@@ -130,6 +131,7 @@ class AugmentedLagrangianQuasiNewton(AugmentedLagrangian):
 
     def __init__(self, nlp, **kwargs):
         AugmentedLagrangian.__init__(self, nlp, **kwargs)
+
 
     def hprod(self, x, z, v, **kwargs):
         """
@@ -139,14 +141,17 @@ class AugmentedLagrangianQuasiNewton(AugmentedLagrangian):
         w = self.Hessapp.matvec(v)
         return w
 
+
     def hupdate(self, new_s=None, new_y=None):
         if new_s is not None and new_y is not None:
             self.Hessapp.store(new_s,new_y)
         return
 
+
     def hreset(self):
         self.Hessapp.restart()
         return
+
 
 
 class AugmentedLagrangianLbfgs(AugmentedLagrangianQuasiNewton):
@@ -180,10 +185,19 @@ class AugmentedLagrangianPartialQuasiNewton(AugmentedLagrangianQuasiNewton):
 
 
     def hprod(self, x, z, v, **kwargs):
-        w = self.Hessapp.matvec(v)
+        on = self.nlp.original_n
+        w = np.zeros(self.n)
+        w[:on] = self.Hessapp.matvec(v[:on])
         J = self.nlp.jac(x)
         w += self.rho * (J.T * (J * v))
         return w
+
+
+    def hupdate(self, new_s=None, new_y=None):
+        on = self.nlp.original_n
+        if new_s is not None and new_y is not None:
+            self.Hessapp.store(new_s[:on],new_y[:on])
+        return
 
 
 
@@ -194,7 +208,7 @@ class AugmentedLagrangianPartialLbfgs(AugmentedLagrangianPartialQuasiNewton):
     """
     def __init__(self, nlp, **kwargs):
         AugmentedLagrangianPartialQuasiNewton.__init__(self, nlp, **kwargs)
-        self.Hessapp = LBFGS(self.n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
+        self.Hessapp = LBFGS(self.nlp.original_n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
 
 
 
@@ -205,7 +219,7 @@ class AugmentedLagrangianPartialLsr1(AugmentedLagrangianPartialQuasiNewton):
     """
     def __init__(self, nlp, **kwargs):
         AugmentedLagrangianPartialQuasiNewton.__init__(self, nlp, **kwargs)
-        self.Hessapp = LSR1(self.n, npairs=kwargs.get('qn_pairs',5), scaling=False, **kwargs)
+        self.Hessapp = LSR1(self.nlp.original_n, npairs=kwargs.get('qn_pairs',5), scaling=False, **kwargs)
 
 
 
@@ -226,7 +240,9 @@ class AugmentedLagrangianTotalQuasiNewton(AugmentedLagrangianPartialQuasiNewton)
 
 
     def hprod(self, x, z, v, **kwargs):
-        w = self.Hessapp.matvec(v)
+        on = self.nlp.original_n
+        w = np.zeros(self.n)
+        w[:on] = self.Hessapp.matvec(v[:on])
         J = self.jac(x)
         w += self.rho * (J.T * (J * v))
         return w
@@ -251,7 +267,7 @@ class AugmentedLagrangianTotalLbfgsAdjBroyA(AugmentedLagrangianTotalQuasiNewton)
     """
     def __init__(self, nlp, **kwargs):
         AugmentedLagrangianTotalQuasiNewton.__init__(self, nlp, **kwargs)
-        self.Hessapp = LBFGS(self.n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
+        self.Hessapp = LBFGS(self.nlp.original_n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
         self.Jacapp = adjointBroydenA(self.nlp.m, self.n, self.x0, self.nlp.cons, 
             self.nlp.jprod, self.nlp.jtprod, slack_index=self.nlp.original_n, **kwargs)
         self.Jacapp.restart(self.x0)
@@ -265,7 +281,7 @@ class AugmentedLagrangianTotalLbfgsAdjBroyB(AugmentedLagrangianTotalQuasiNewton)
     """
     def __init__(self, nlp, **kwargs):
         AugmentedLagrangianTotalQuasiNewton.__init__(self, nlp, **kwargs)
-        self.Hessapp = LBFGS(self.n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
+        self.Hessapp = LBFGS(self.nlp.original_n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
         self.Jacapp = adjointBroydenB(self.nlp.m, self.n, self.x0, self.nlp.cons, 
             self.nlp.jprod, self.nlp.jtprod, slack_index=self.nlp.original_n, **kwargs)
         self.Jacapp.restart(self.x0)
@@ -279,7 +295,7 @@ class AugmentedLagrangianTotalLbfgsAdjBroyC(AugmentedLagrangianTotalQuasiNewton)
     """
     def __init__(self, nlp, **kwargs):
         AugmentedLagrangianTotalQuasiNewton.__init__(self, nlp, **kwargs)
-        self.Hessapp = LBFGS(self.n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
+        self.Hessapp = LBFGS(self.nlp.original_n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
         self.Jacapp = adjointBroydenC(self.nlp.m, self.n, self.x0, self.nlp.cons, 
             self.nlp.jprod, self.nlp.jtprod, slack_index=self.nlp.original_n, **kwargs)
         self.Jacapp.restart(self.x0)
@@ -293,7 +309,7 @@ class AugmentedLagrangianTotalLbfgsTR1A(AugmentedLagrangianTotalQuasiNewton):
     """
     def __init__(self, nlp, **kwargs):
         AugmentedLagrangianTotalQuasiNewton.__init__(self, nlp, **kwargs)
-        self.Hessapp = LBFGS(self.n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
+        self.Hessapp = LBFGS(self.nlp.original_n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
         self.Jacapp = TR1A(self.nlp.m, self.n, self.x0, self.nlp.cons, 
             self.nlp.jprod, self.nlp.jtprod, slack_index=self.nlp.original_n, **kwargs)
         self.Jacapp.restart(self.x0)
@@ -307,7 +323,7 @@ class AugmentedLagrangianTotalLbfgsTR1B(AugmentedLagrangianTotalQuasiNewton):
     """
     def __init__(self, nlp, **kwargs):
         AugmentedLagrangianTotalQuasiNewton.__init__(self, nlp, **kwargs)
-        self.Hessapp = LBFGS(self.n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
+        self.Hessapp = LBFGS(self.nlp.original_n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
         self.Jacapp = TR1B(self.nlp.m, self.n, self.x0, self.nlp.cons, 
             self.nlp.jprod, self.nlp.jtprod, slack_index=self.nlp.original_n, **kwargs)
         self.Jacapp.restart(self.x0)
@@ -321,7 +337,7 @@ class AugmentedLagrangianTotalLbfgsBroyden(AugmentedLagrangianTotalQuasiNewton):
     """
     def __init__(self, nlp, **kwargs):
         AugmentedLagrangianTotalQuasiNewton.__init__(self, nlp, **kwargs)
-        self.Hessapp = LBFGS(self.n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
+        self.Hessapp = LBFGS(self.nlp.original_n, npairs=kwargs.get('qn_pairs',1), scaling=True, **kwargs)
         self.Jacapp = Broyden(self.nlp.m, self.n, self.x0, self.nlp.cons, 
             self.nlp.jprod, self.nlp.jtprod, slack_index=self.nlp.original_n, **kwargs)
         self.Jacapp.restart(self.x0)