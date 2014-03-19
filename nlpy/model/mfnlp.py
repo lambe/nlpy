Fixed a Tron bug: we want the Hessian to stay constant until x changes. (Bound-constrained QP) Data storage can be safely reintroduced.

nlpy/model/mfnlp.py
@@ -160,11 +160,6 @@ class SlackNLP( MFModel ):
         # Numpy norm calculation is much faster for long arrays
         self._cache = {'x':np.infty * np.ones(self.original_n,'d'),
             'obj':None, 'cons':None, 'grad':None}
-        # self._last_x = np.infty * np.ones(self.original_n,'d')
-        # self._last_x_hash = hashlib.sha1(self._last_x).hexdigest()
-        # self._last_obj = None
-        # self._last_cons = None
-        # self._last_grad = None
 
         return
 
@@ -185,27 +180,22 @@ class SlackNLP( MFModel ):
         subvector of `x`.
         """
 
-        # x_hash = hashlib.sha1(x[:self.original_n]).hexdigest()
-        # same_x = self._last_x_hash == x_hash
-        # same_x = (self._last_x == x[:self.original_n]).all()
-
-        # pdb.set_trace()
         same_x = norm(x[:self.original_n] - self._cache['x']) < eps
 
-        # if self._cache['obj'] is not None and same_x:
-        #     f = self._cache['obj']
-        # elif self._cache['obj'] is None and same_x:
-        #     f = self.nlp.obj(self._cache['x'])
-        #     self._cache['obj'] = copy.deepcopy(f)
-        # else:
-        #     f = self.nlp.obj(x[:self.original_n])
-        #     self._cache['x'] = x[:self.original_n].copy()
-        #     self._cache['obj'] = copy.deepcopy(f)
-        #     self._cache['cons'] = None
-        #     self._cache['grad'] = None
+        if self._cache['obj'] is not None and same_x:
+            f = self._cache['obj']
+        elif self._cache['obj'] is None and same_x:
+            f = self.nlp.obj(self._cache['x'])
+            self._cache['obj'] = copy.deepcopy(f)
+        else:
+            f = self.nlp.obj(x[:self.original_n])
+            self._cache['x'] = x[:self.original_n].copy()
+            self._cache['obj'] = copy.deepcopy(f)
+            self._cache['cons'] = None
+            self._cache['grad'] = None
 
         # Debug mode
-        f = self.nlp.obj(x[:self.original_n])
+        # f = self.nlp.obj(x[:self.original_n])
 
         return f
 
@@ -219,24 +209,20 @@ class SlackNLP( MFModel ):
         g = np.zeros(self.n)
         same_x = norm(x[:self.original_n] - self._cache['x']) < eps
 
-        # x_hash = hashlib.sha1(x[:self.original_n]).hexdigest()
-        # same_x = self._last_x_hash == x_hash
-        # same_x = (self._last_x == x[:self.original_n]).all()
-
-        # if self._cache['grad'] is not None and same_x:
-        #     g[:self.original_n] = self._cache['grad']
-        # elif self._cache['grad'] is None and same_x:
-        #     g[:self.original_n] = self.nlp.grad(self._cache['x'])
-        #     self._cache['grad'] = copy.deepcopy(g[:self.original_n])
-        # else:
-        #     g[:self.original_n] = self.nlp.grad(x[:self.original_n])
-        #     self._cache['x'] = x[:self.original_n].copy()
-        #     self._cache['obj'] = None
-        #     self._cache['cons'] = None
-        #     self._cache['grad'] = copy.deepcopy(g[:self.original_n])
+        if self._cache['grad'] is not None and same_x:
+            g[:self.original_n] = self._cache['grad']
+        elif self._cache['grad'] is None and same_x:
+            g[:self.original_n] = self.nlp.grad(self._cache['x'])
+            self._cache['grad'] = g[:self.original_n].copy()
+        else:
+            g[:self.original_n] = self.nlp.grad(x[:self.original_n])
+            self._cache['x'] = x[:self.original_n].copy()
+            self._cache['obj'] = None
+            self._cache['cons'] = None
+            self._cache['grad'] = g[:self.original_n].copy()
 
         # Debug mode
-        g[:self.original_n] = self.nlp.grad(x[:self.original_n])
+        # g[:self.original_n] = self.nlp.grad(x[:self.original_n])
 
         return g
 
@@ -277,24 +263,20 @@ class SlackNLP( MFModel ):
         c = np.empty(m)
         same_x = norm(x[:on] - self._cache['x']) < eps
 
-        # x_hash = hashlib.sha1(x[:self.original_n]).hexdigest()
-        # same_x = self._last_x_hash == x_hash
-        # same_x = (self._last_x == x[:self.original_n]).all()
-
-        # if self._cache['cons'] is not None and same_x:
-        #     c[:om] = self._cache['cons']
-        # elif self._cache['cons'] is None and same_x:
-        #     c[:om] = self.nlp.cons(self._cache['x'])
-        #     self._cache['cons'] = copy.deepcopy(c[:om])
-        # else:
-        #     c[:om] = self.nlp.cons(x[:on])
-        #     self._cache['x'] = x[:on]
-        #     self._cache['obj'] = None
-        #     self._cache['cons'] = copy.deepcopy(c[:om])
-        #     self._cache['grad'] = None
+        if self._cache['cons'] is not None and same_x:
+            c[:om] = self._cache['cons']
+        elif self._cache['cons'] is None and same_x:
+            c[:om] = self.nlp.cons(self._cache['x'])
+            self._cache['cons'] = c[:om].copy()
+        else:
+            c[:om] = self.nlp.cons(x[:on])
+            self._cache['x'] = x[:on].copy()
+            self._cache['obj'] = None
+            self._cache['cons'] = c[:om].copy()
+            self._cache['grad'] = None
 
         # Debug mode
-        c[:om] = self.nlp.cons(x[:on])
+        # c[:om] = self.nlp.cons(x[:on])
 
         c[om:om+nrangeC] = c[rangeC]
 